6bff04701eff2f225084fe974a85b67ec862bc17
LST <wer1264@gmail.com>
2022-11-16 18:04:53 +0900

    Update: 로그인 ãc시 해시화된 비밀번호 비êµ를 비교하여 검증

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -76,12 +76,27 @@ const getLogin = (req, res) => {
 
 const postLogin = async (req, res) => {
   const { id, password } = req.body;
+
   try {
-    await User.create({
-      id,
-      password,
-    });
-    res.render('login');
+    const user = await User.findOne({ id });
+
+    if (!user) {
+      res.status(400).render('login', {
+        errorMessage: '존재하지 않는 유저입니다.',
+      });
+      return;
+    }
+
+    const pwCompare = await bcrypt.compare(password, user.password);
+
+    if (!pwCompare) {
+      res.status(400).render('login', {
+        errorMessage: '비밀번호가 틀렸습니다.',
+      });
+      return;
+    }
+
+    res.render('home');
   } catch (e) {
     res.status(404).render('login', {
       errorMessage: e.message,
