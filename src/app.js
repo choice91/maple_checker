918f5d6c734721aa28657b1fe50157d0f43a8dbe
LST <wer1264@gmail.com>
2022-11-17 17:28:05 +0900

    Add: Session 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const routes = require('./routes/index');
 
@@ -12,13 +14,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
 
 app.use('/static', express.static(path.join(__dirname, 'client')));
-
 app.use('/', routes);
-
-app.get('/', (req, res, next) => {
-  res.render('login');
-});
 
 module.exports = app;
