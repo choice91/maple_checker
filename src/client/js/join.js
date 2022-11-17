2619df9070af2eb06fe1023fdc96abaf53c7fd03
LST <wer1264@gmail.com>
2022-11-17 16:30:45 +0900

    Update: 아이디중복검사

--- src/client/js/join.js
+++ src/client/js/join.js
@@ -1,5 +1,5 @@
 const $id = document.querySelector('.join-container__id');
-const $idDuplicationBlock = document.querySelector('.join-container__id-msg');
+const $idMsg = document.querySelector('.join-container__id-msg');
 const $pw = document.querySelector('.join-container__pw');
 const $pw2 = document.querySelector('.join-container__pw2');
 const $name = document.querySelector('.join-container__name');
@@ -8,11 +8,11 @@ const checkId = async () => {
   const regExp = /^[a-z]+[a-z0-9]{4,19}$/g;
 
   if (!$id.value) {
-    $idDuplicationBlock.innerText = '아이디를 입력해주세요.';
+    $idMsg.innerText = '아이디를 입력해주세요.';
   } else if ($id.value.length < 5) {
-    $idDuplicationBlock.innerText = '아이디를 5글자 이상 입력해주세요.';
+    $idMsg.innerText = '아이디를 5글자 이상 입력해주세요.';
   } else if (!regExp.test($id.value)) {
-    $idDuplicationBlock.innerText =
+    $idMsg.innerText =
       '영문자로 시작하는 영문자 또는 숫자 5~20자로 입력해주세요';
   } else {
     const response = await fetch('/id-check', {
@@ -26,31 +26,46 @@ const checkId = async () => {
     });
 
     if (response.status === 409) {
-      $idDuplicationBlock.innerText = '아이디가 중복입니다.';
+      $idMsg.innerText = '아이디가 중복입니다.';
     } else {
-      $idDuplicationBlock.innerText = '사용할 수 있는 아이디입니다.';
+      $idMsg.innerText = '사용할 수 있는 아이디입니다.';
     }
   }
 };
 
-const submitJoin = () => {
+const submitJoin = async (e) => {
   if (!$id.value) {
+    e.preventDefault();
     alert('아이디를 입력해주세요.');
+    $id.focus();
+    return false;
+  }
+
+  if ($idMsg.textContent === '아이디가 중복입니다.') {
+    e.preventDefault();
+    alert('아이디가 중복입니다. 다른 아이디를 사용해주세요.');
+    $id.focus();
     return false;
   }
 
   if (!$pw.value) {
+    e.preventDefault();
     alert('비밀번호를 입력해주세요.');
+    $pw.focus();
     return false;
   }
 
   if (!$pw2.value) {
+    e.preventDefault();
     alert('비밀번호 확인을 입력해주세요.');
+    $pw2.focus();
     return false;
   }
 
   if (!$name.value) {
+    e.preventDefault();
     alert('이름을 입력해주세요.');
+    $name.focus();
     return false;
   }
 
--- src/routes/root.js
+++ src/routes/root.js
@@ -6,12 +6,23 @@ const {
   idCheck,
   getLogin,
   postLogin,
+  logout,
 } = require('../controllers/user');
+const { home } = require('../controllers/home');
 
 const rootRouter = express.Router();
 
-rootRouter.route('/login').get(getLogin).post(postLogin);
-rootRouter.route('/join').get(getJoin).post(postJoin);
-rootRouter.route('/id-check').post(idCheck);
+rootRouter.get('/', getLogin);
+
+rootRouter.get('/login', getLogin);
+rootRouter.post('/login', postLogin);
+rootRouter.get('/logout', logout);
+
+rootRouter.get('/join', getJoin);
+rootRouter.post('/join', postJoin);
+
+rootRouter.post('/id-check', idCheck);
+
+rootRouter.get('/home', home);
 
 module.exports = rootRouter;
