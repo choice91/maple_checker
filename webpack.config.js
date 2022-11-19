c726066abc48dae2ebf82e5ecd567b7dbb580ab3
LST <wer1264@gmail.com>
2022-11-19 23:15:29 +0900

    Add: Babel and Webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const BASE_JS = './src/client/js/';
+
+module.exports = {
+  entry: {
+    main: BASE_JS + 'main.js',
+    join: BASE_JS + 'join.js',
+    login: BASE_JS + 'login.js',
+  },
+  // mode: 'development',
+  // watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',
+    }),
+  ],
+  output: {
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'assets'),
+    // clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+};
