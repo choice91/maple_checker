d5c109af329a36ce9b2ce7b48315dc087e24b7a3
LST <wer1264@gmail.com>
2022-11-29 21:54:31 +0900

    Add: schema option 추가

--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,25 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 const User = mongoose.model('User', userSchema);
 
