31265a8a3cef77a0a6e72d8730dc9f9dd134170b
LST <wer1264@gmail.com>
2022-11-29 15:41:16 +0900

    Update: eslint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    ecmaVersion: 2022,
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   env: {
     node: true,
   },
-  extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
+  extends: ['airbnb-base', 'plugin:import/recommended', 'prettier'],
+  rules: {
+    'import/prefer-default-export': 0,
+    'no-underscore-dangle': 0,
+  },
 };
