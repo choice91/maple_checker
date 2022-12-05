69a466756902f32b4c4ea93e1b28149cd3a5a0a4
LST <wer1264@gmail.com>
2022-12-05 15:53:07 +0900

    Add: 스케줄러 추가

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import 'dotenv/config';
 import './db';
 import app from './app';
+import scheduler from './scheduler';
 
 const { PORT } = process.env;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  scheduler.start();
+});
