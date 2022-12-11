141c311d63e33c88f1c3a449526851570619b0e6
LST <wer1264@gmail.com>
2022-12-11 23:08:34 +0900

    Update: 클라이언트 로그인 함수 수정

--- client/src/redux/async/user.js
+++ client/src/redux/async/user.js
@@ -1,22 +1,69 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import instance from '../apis';
-import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
 export const login = createAsyncThunk(
   'user/login',
   async (payload, thunkAPI) => {
-    const { id, pw } = payload;
+    const {
+      data: { id, pw },
+      navigate,
+    } = payload;
 
     try {
       const response = await instance.post('/login', {
         id,
         password: pw,
       });
+
+      const { accessToken } = response.data.token;
+      const decodedToken = jwtDecode(accessToken);
+
+      localStorage.setItem(
+        'user',
+        JSON.stringify({
+          id: decodedToken.id,
+          username: decodedToken.username,
+          token: accessToken,
+        })
+      );
+
+      navigate('/', { replace: true });
+
+      return { id, username: decodedToken.username };
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const signUp = createAsyncThunk(
+  'user/signUp',
+  async (payload, thunkAPI) => {
+    const { id, pw, pw2, name } = payload;
+
+    try {
+      const response = await instance.post('/join', {
+        id,
+        password: pw,
+        password2: pw2,
+        name,
+      });
       console.log(response);
     } catch (err) {
-      if (err) {
-        console.error(err.response);
-      }
+      return thunkAPI.rejectWithValue(err.response.data);
+    }
+  }
+);
+
+export const loginCheck = createAsyncThunk(
+  'user/loginCheck',
+  async (payload, thunkAPI) => {
+    try {
+      const response = await instance.get('/auth-check');
+      return response.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue(err.response.data);
     }
   }
 );
