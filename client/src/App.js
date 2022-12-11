e98142ffbac95aa3ba947f7950e1d0aa804c0a83
LST <wer1264@gmail.com>
2022-12-11 18:12:15 +0900

    Update: 인증되지 않으면 로그인 화면으로 redirect 시키도록 router 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Login from './pages/login';
+
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
+import RequireAuth from './utils/RequireAuth';
 
 import './css/default.scss';
 
 function App() {
   return (
-    <div className="main">
+    <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth redirectTo="/login">
+              <Main />
+            </RequireAuth>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
