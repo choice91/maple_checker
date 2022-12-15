c87bee090c39e87cb6632ea487c6bd9e3ef26be3
LST <wer1264@gmail.com>
2022-12-15 17:54:55 +0900

    Add: (client) create, delete icon 추가

--- client/src/components/TableTitle.js
+++ client/src/components/TableTitle.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import CreateIcon from '@mui/icons-material/Create';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 const TableTitle = ({ quests }) => {
+  const updateCharacter = (e) => {};
+
+  const deleteCharacter = (e) => {};
+
   return (
     <>
       <th>&nbsp;</th>
       {quests.map(({ nickname }, index) => (
-        <th key={index}>{nickname}</th>
+        <th key={index}>
+          <span>{nickname}</span>
+          <div className="icons">
+            <span className="icons__update-btn" onClick={updateCharacter}>
+              <CreateIcon />
+            </span>
+            <span className="icons__delete-btn" onClick={deleteCharacter}>
+              <DeleteIcon />
+            </span>
+          </div>
+        </th>
       ))}
     </>
   );
