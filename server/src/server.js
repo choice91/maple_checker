5af249d01b3422dd2abd2e1c0e3a5ea8dbf48956
LST <wer1264@gmail.com>
2022-12-24 23:49:56 +0900

    Add: (server) Boss data reset scheduler 추가

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,12 @@
 import 'dotenv/config';
 import './db';
 import app from './app';
-import scheduler from './scheduler';
+import { questResetScheduler, bossResetScheduler } from './scheduler';
 
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  scheduler.start();
+  questResetScheduler.start();
+  bossResetScheduler.start();
 });
