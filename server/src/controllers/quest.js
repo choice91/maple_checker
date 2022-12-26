2978d8aac804a4bf72536a56c8629e7daa6867e0
LST <wer1264@gmail.com>
2022-12-26 16:42:20 +0900

    Update: (server) 캐릭터 삭제 실패시 response값 수정 (quest)
    message -> errorMessage

--- server/src/controllers/quest.js
+++ server/src/controllers/quest.js
@@ -82,7 +82,7 @@ export const deleteCharacter = async (req, res) => {
   if (response.deletedCount !== 1) {
     res.status(400).json({
       ok: false,
-      message: '삭제중 오류가 발생했습니다. 다시 시도해주세요.',
+      errorMessage: '삭제중 오류가 발생했습니다. 다시 시도해주세요.',
     });
     return;
   }
