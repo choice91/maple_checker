e28472033025e472f8bc9ce47ff5ecf3e49e2434
LST <wer1264@gmail.com>
2022-12-28 22:53:13 +0900

    Update: (server) root router에서 auth controller 이름 수정

--- server/src/routes/root.js
+++ server/src/routes/root.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { postJoin, idCheck, postLogin, logout } from '../controllers/user';
-import auth from '../controllers/auth';
+import authCtrl from '../controllers/auth';
 import { home } from '../controllers/home';
 import { asyncHandler, isLoggedIn } from '../middlewares';
 
@@ -12,6 +12,6 @@ rootRouter.route('/login').post(asyncHandler(postLogin));
 rootRouter.route('/logout').get(logout);
 rootRouter.route('/join').post(asyncHandler(postJoin));
 rootRouter.route('/id-check').post(asyncHandler(idCheck));
-rootRouter.route('/refresh').get(asyncHandler(auth.reissueToken));
+rootRouter.route('/refresh').get(asyncHandler(authCtrl.reissueToken));
 
 export default rootRouter;
