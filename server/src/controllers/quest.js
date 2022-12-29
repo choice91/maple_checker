9a751239b1d6a30381bfecafbae6e7c0679372a8
LST <wer1264@gmail.com>
2022-12-29 13:36:27 +0900

    Refactor: (server) Controller를 export default로 내보내도록 수정

--- server/src/controllers/quest.js
+++ server/src/controllers/quest.js
@@ -2,167 +2,169 @@ import db from '../models';
 
 import { arrayToObjectFn } from '../service/functions';
 
-export const addCharacter = async (req, res) => {
-  const {
-    user: { id: loginUserId },
-    body: { nickname },
-  } = req;
+export default {
+  addCharacter: async (req, res) => {
+    const {
+      user: { id: loginUserId },
+      body: { nickname },
+    } = req;
 
-  const quest = await db.Quest.findOne({ owner: loginUserId, nickname });
+    const quest = await db.Quest.findOne({ owner: loginUserId, nickname });
 
-  // 이미 캐릭터가 등록되어 있는 경우
-  if (quest) {
-    res.status(400).json({
-      ok: false,
-      message: '이미 등록된 캐릭터입니다.',
+    // 이미 캐릭터가 등록되어 있는 경우
+    if (quest) {
+      res.status(400).json({
+        ok: false,
+        message: '이미 등록된 캐릭터입니다.',
+      });
+      return;
+    }
+
+    // 캐릭터 등록
+    const newCharacter = await db.Quest.create({
+      owner: loginUserId,
+      nickname,
     });
-    return;
-  }
 
-  // 캐릭터 등록
-  const newCharacter = await db.Quest.create({
-    owner: loginUserId,
-    nickname,
-  });
+    const result = {
+      [newCharacter._id]: {
+        nickname: newCharacter.nickname,
+        owner: newCharacter.owner,
+        quests: newCharacter.quests,
+      },
+    };
 
-  const result = {
-    [newCharacter._id]: {
-      nickname: newCharacter.nickname,
-      owner: newCharacter.owner,
-      quests: newCharacter.quests,
-    },
-  };
-
-  res.status(200).json({
-    ok: true,
-    message: '캐릭터 추가 완료',
-    newCharacter: result,
-  });
-};
-
-export const updateNickname = async (req, res) => {
-  const {
-    user: { id: loginUserId },
-    body: { prevNickname, newNickname },
-  } = req;
-
-  const character = await db.Quest.findOne({
-    owner: loginUserId,
-    nickname: prevNickname,
-  });
-
-  if (!character) {
-    res.status(404).json({
-      ok: false,
-      errorMessage: '존재하지 않는 캐릭터',
+    res.status(200).json({
+      ok: true,
+      message: '캐릭터 추가 완료',
+      newCharacter: result,
     });
-    return;
-  }
+  },
 
-  character.nickname = newNickname;
-  await character.save();
+  updateNickname: async (req, res) => {
+    const {
+      user: { id: loginUserId },
+      body: { prevNickname, newNickname },
+    } = req;
 
-  res.status(200).json({
-    ok: true,
-    message: '닉네임 변경 성공',
-  });
-};
-
-export const deleteCharacter = async (req, res) => {
-  const {
-    user: { id: loginUserId },
-    params: { questId },
-  } = req;
-
-  const response = await db.Quest.deleteOne({
-    _id: questId,
-    owner: loginUserId,
-  });
-
-  if (response.deletedCount !== 1) {
-    res.status(400).json({
-      ok: false,
-      errorMessage: '삭제중 오류가 발생했습니다. 다시 시도해주세요.',
+    const character = await db.Quest.findOne({
+      owner: loginUserId,
+      nickname: prevNickname,
     });
-    return;
-  }
 
-  res.status(200).json({
-    ok: true,
-    message: '캐릭터 삭제',
-  });
-};
+    if (!character) {
+      res.status(404).json({
+        ok: false,
+        errorMessage: '존재하지 않는 캐릭터',
+      });
+      return;
+    }
 
-export const getDailyQuest = async (req, res) => {
-  const { id: loginUserId } = req.user;
+    character.nickname = newNickname;
+    await character.save();
 
-  const quest = await db.Quest.find({ owner: loginUserId });
-  const questObj = arrayToObjectFn(quest);
-
-  res.status(200).json({
-    ok: true,
-    quests: questObj,
-  });
-};
-
-export const questComplete = async (req, res) => {
-  const {
-    user: { id: loginUserId },
-    body: { nickname, questType },
-  } = req;
-
-  const quest = await db.Quest.findOne({ owner: loginUserId, nickname });
-
-  if (!quest) {
-    res.status(404).json({
-      ok: false,
-      message: '존재하지 않는 캐릭터입니다.',
+    res.status(200).json({
+      ok: true,
+      message: '닉네임 변경 성공',
     });
-    return;
-  }
+  },
 
-  quest.quests[`${questType}`] = !quest.quests[`${questType}`];
-  await quest.save();
+  deleteCharacter: async (req, res) => {
+    const {
+      user: { id: loginUserId },
+      params: { questId },
+    } = req;
 
-  res.status(200).json({
-    ok: true,
-    message: quest.quests[`${questType}`] ? '완료' : '취소',
-  });
-};
-
-export const resetQuestData = async (req, res) => {
-  const {
-    user: { id: loginUserId },
-  } = req;
-
-  const questDefaultValues = {
-    yeoro: false,
-    chuchu: false,
-    lachelein: false,
-    arcana: false,
-    morass: false,
-    esfera: false,
-    cernium: false,
-    burningCernium: false,
-    arcs: false,
-    odium: false,
-  };
-
-  const updateResponse = await db.Quest.updateMany(
-    { owner: loginUserId },
-    { $set: { quests: questDefaultValues } }
-  );
-
-  if (updateResponse.modifiedCount === 0) {
-    res.status(400).json({
-      ok: false,
-      errorMessage: '초기화 에러',
+    const response = await db.Quest.deleteOne({
+      _id: questId,
+      owner: loginUserId,
     });
-    return;
-  }
 
-  res.status(200).json({
-    ok: true,
-    message: '퀘스트 데이터 초기화',
-  });
+    if (response.deletedCount !== 1) {
+      res.status(400).json({
+        ok: false,
+        errorMessage: '삭제중 오류가 발생했습니다. 다시 시도해주세요.',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      message: '캐릭터 삭제',
+    });
+  },
+
+  getQuestData: async (req, res) => {
+    const { id: loginUserId } = req.user;
+
+    const quest = await db.Quest.find({ owner: loginUserId });
+    const questObj = arrayToObjectFn(quest);
+
+    res.status(200).json({
+      ok: true,
+      quests: questObj,
+    });
+  },
+
+  questComplete: async (req, res) => {
+    const {
+      user: { id: loginUserId },
+      body: { nickname, questType },
+    } = req;
+
+    const quest = await db.Quest.findOne({ owner: loginUserId, nickname });
+
+    if (!quest) {
+      res.status(404).json({
+        ok: false,
+        message: '존재하지 않는 캐릭터입니다.',
+      });
+      return;
+    }
+
+    quest.quests[`${questType}`] = !quest.quests[`${questType}`];
+    await quest.save();
+
+    res.status(200).json({
+      ok: true,
+      message: quest.quests[`${questType}`] ? '완료' : '취소',
+    });
+  },
+
+  resetQuestData: async (req, res) => {
+    const {
+      user: { id: loginUserId },
+    } = req;
+
+    const questDefaultValues = {
+      yeoro: false,
+      chuchu: false,
+      lachelein: false,
+      arcana: false,
+      morass: false,
+      esfera: false,
+      cernium: false,
+      burningCernium: false,
+      arcs: false,
+      odium: false,
+    };
+
+    const updateResponse = await db.Quest.updateMany(
+      { owner: loginUserId },
+      { $set: { quests: questDefaultValues } }
+    );
+
+    if (updateResponse.modifiedCount === 0) {
+      res.status(400).json({
+        ok: false,
+        errorMessage: '초기화 에러',
+      });
+      return;
+    }
+
+    res.status(200).json({
+      ok: true,
+      message: '퀘스트 데이터 초기화',
+    });
+  },
 };
