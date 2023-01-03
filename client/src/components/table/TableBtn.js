cf773c96d92798ed25030d6126184468f350260e
LST <wer1264@gmail.com>
2023-01-03 16:54:25 +0900

    Refactor: (client) TableBtn 컴포넌트 파일 구조 수정

---
+++ client/src/components/table/TableBtn.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import questSlice from '../../redux/slices/questSlice';
+import bossSlice from '../../redux/slices/bossSlice';
+import { resetQuestData } from '../../redux/async/quest';
+import { resetBossData } from '../../redux/async/boss';
+
+import { Box, ButtonGroup, Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fff',
+      gray: '#191919',
+    },
+  },
+});
+
+const TableBtn = ({ page, setAddModalOpen }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const openAddModal = () => {
+    if (page === 'todo') {
+      // dispatch(questSlice.actions.openQuestAddModal());
+      setAddModalOpen(true);
+    } else {
+      dispatch(bossSlice.actions.openBossAddModal());
+    }
+  };
+
+  const resetData = () => {
+    if (page === 'todo') {
+      dispatch(resetQuestData({ navigate }));
+    } else {
+      dispatch(resetBossData({ navigate }));
+    }
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ backgroundColor: 'primary.gray' }}>
+          <ButtonGroup>
+            <Button startIcon={<PersonAddAltIcon />} onClick={openAddModal}>
+              캐릭터 추가
+            </Button>
+            <Button startIcon={<RestartAltIcon />} onClick={resetData}>
+              리셋
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </ThemeProvider>
+      {/*<div className="table-btn">*/}
+      {/*  <button onClick={openAddModal}>*/}
+      {/*    <PersonAddAltIcon />*/}
+      {/*    <span>캐릭터 추가</span>*/}
+      {/*  </button>*/}
+      {/*  <button onClick={resetData}>*/}
+      {/*    <RestartAltIcon />*/}
+      {/*    <span>리셋</span>*/}
+      {/*  </button>*/}
+      {/*</div>*/}
+    </>
+  );
+};
+
+export default TableBtn;
