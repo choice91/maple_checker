c91197cdee6c5aa2a1f85fed3641663e44e6f0de
LST <wer1264@gmail.com>
2023-01-03 16:55:10 +0900

    Update: (client) TableTitle 컴포넌트에 Material UI 적용

--- client/src/components/table/TableTitle.js
+++ client/src/components/table/TableTitle.js
@@ -1,21 +1,39 @@
 import React from 'react';
 
-import TableTitleBtn from './TableTitleBtn';
+import { Box, TableRow } from '@mui/material';
 
-const TableTitle = ({ ids, data, type }) => {
+import CustomTableCell from './CustomTableCell';
+import TableMoreBtn from './TableMoreBtn';
+
+const TableTitle = ({ ids, data, page }) => {
   return (
     <>
-      <th>&nbsp;</th>
-      {ids.map((id, index) => (
-        <th key={index}>
-          <span>{data[`${id}`].nickname}</span>
-          <TableTitleBtn
-            nickname={data[`${id}`].nickname}
-            id={id}
-            type={type}
-          />
-        </th>
-      ))}
+      <TableRow>
+        <CustomTableCell
+          bgColor="#212121"
+          fontColor="#fff"
+          width={90}
+          minWidth={90}
+          fontWeight={700}
+          align="center"
+        >
+          &nbsp;
+        </CustomTableCell>
+        {ids.map((id, index) => (
+          <CustomTableCell
+            key={index}
+            bgColor="#212121"
+            fontColor="#fff"
+            fontSize={16}
+            minWidth={115}
+            fontWeight={700}
+            align="center"
+          >
+            <Box component="span">{data[id].nickname}</Box>
+            <TableMoreBtn id={id} nickname={data[id].nickname} />
+          </CustomTableCell>
+        ))}
+      </TableRow>
     </>
   );
 };
