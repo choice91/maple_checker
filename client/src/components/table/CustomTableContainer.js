f776895f6ee341ef7490e6c747b7d4a8ee1a5b5c
LST <wer1264@gmail.com>
2023-01-05 22:47:55 +0900

    Refactor: (client) 테이블 공통 container 리팩토링

---
+++ client/src/components/table/CustomTableContainer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Container, Paper, TableContainer } from '@mui/material';
+import TableBtn from './TableBtn';
+
+const CustomTableContainer = ({ page, children }) => {
+  return (
+    <>
+      <Container
+        component="main"
+        maxWidth="lg"
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          mt: 10,
+        }}
+      >
+        <TableBtn page={page} />
+        <TableContainer
+          component={Paper}
+          sx={{
+            mb: 10,
+            height: 650,
+            '&::-webkit-scrollbar': {
+              width: 10,
+            },
+            '&::-webkit-scrollbar-track': {
+              backgroundColor: '#212121',
+            },
+            '&::-webkit-scrollbar-thumb': {
+              backgroundColor: '#7F7F7F',
+              borderRadius: 1,
+            },
+          }}
+        >
+          {children}
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default CustomTableContainer;
