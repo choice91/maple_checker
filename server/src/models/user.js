ed32c5f642418d5328a6f9e3fdf850d3d8dd007e
LST <wer1264@gmail.com>
2023-01-11 17:30:22 +0900

    Add: (server) User schema에 새로운 속성 추가

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -2,22 +2,12 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
   {
-    id: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    refreshToken: {
-      type: String,
-    },
+    id: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    refreshToken: { type: String },
+    todoSeq: { type: [mongoose.Schema.Types.ObjectId], ref: 'Todo' },
+    bossSeq: { type: [mongoose.Schema.Types.ObjectId], ref: 'Boss' },
   },
   {
     versionKey: false,
