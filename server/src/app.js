d87bebec285f54f993ba3bec250bd5fc9ab8ce9e
LST <wer1264@gmail.com>
2023-01-16 22:02:00 +0900

    Add: (server) 배포용 미들웨어 추가

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
 import routes from './routes';
 
 const app = express();
 
-const whitelist = ['http://localhost:3000'];
+const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
 const corsOptions = {
   origin(origin, callback) {
     if (whitelist.indexOf(origin) >= 0 || !origin) {
@@ -22,11 +23,17 @@ app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.use('/', routes);
 
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
 app.use((error, req, res, next) => {
   const errorStatus = error.statusCode || 500;
+  console.log(error);
 
   res.status(errorStatus).json({
     message: 'Server Error',
