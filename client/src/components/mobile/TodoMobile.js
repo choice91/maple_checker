92a1858e21ed2ad5f4b491f556b32b17b3825c72
LST <wer1264@gmail.com>
2023-01-16 22:07:13 +0900

    Update: (client) 모바일 화면에서 캐릭터 추가 모달 뜨도록 수정

--- client/src/components/mobile/TodoMobile.js
+++ client/src/components/mobile/TodoMobile.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Box, Button, ButtonGroup, Grid } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@mui/styles';
+import { Box, Button, ButtonGroup, Grid } from '@mui/material';
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+
+import modalSlice from '../../redux/slices/modalSlice';
+import todoSlice from '../../redux/slices/todoSlice';
 
 import GridItem from './GridItem';
 import TodoSelect from '../table/TodoSelect';
-import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 const useStyles = makeStyles({
   gridContainer: {
@@ -33,15 +36,26 @@ const useStyles = makeStyles({
 
 const TodoMobile = ({ dailyArray, weeklyArray }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
 
   const { todoData, todoSeq, category } = useSelector((state) => state.todo);
+
+  const openAddModal = () => {
+    const args = { page: 'todo' };
+    dispatch(modalSlice.actions.openAddModal(args));
+    dispatch(todoSlice.actions.clearTodoErrorMsg());
+  };
 
   return (
     <>
       <Grid container spacing={1} className={classes.gridContainer}>
         <Box className={classes.box}>
           <ButtonGroup>
-            <Button startIcon={<PersonAddAltIcon />} className={classes.button}>
+            <Button
+              startIcon={<PersonAddAltIcon />}
+              className={classes.button}
+              onClick={openAddModal}
+            >
               캐릭터 추가
             </Button>
             <Button startIcon={<RestartAltIcon />} className={classes.button}>
