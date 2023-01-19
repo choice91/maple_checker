78de2043684e607672b611b633a2ff1e8053aba6
LST <wer1264@gmail.com>
2023-01-19 17:47:32 +0900

    Add: (server) ecosystem.config.js 추가

---
+++ server/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: './src/server.js',
+      instances: 0,
+      exec_mode: 'cluster',
+      watch: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
