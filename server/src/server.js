b2f7b6552cd86bc1d15bb4e83cba983bdd478dfa
LST <wer1264@gmail.com>
2023-01-28 15:45:12 +0900

    Update: (server) scheduler 수정

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,12 @@
 import 'dotenv/config';
 import './db';
 import app from './app';
-import {
-  dailyResetScheduler,
-  monthlyResetScheduler,
-  weeklyResetScheduler,
-} from './scheduler';
+import { monthlyResetScheduler, weeklyResetScheduler } from './scheduler';
 
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  dailyResetScheduler.start();
   weeklyResetScheduler.start();
   monthlyResetScheduler.start();
 });
