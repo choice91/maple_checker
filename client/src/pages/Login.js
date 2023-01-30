926cd6e632290b14e4a49fd7b7e72bb3c87bb6dc
LST <wer1264@gmail.com>
2023-01-30 21:41:47 +0900

    Refactor: (client) 로그인페이지 수정

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,109 +1,119 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Button,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
 
-import { login } from '../redux/async/user';
-import userSlice from '../redux/slices/userSlice';
+import { login } from "../redux/async/user";
+import userSlice from "../redux/slices/userSlice";
 
-import { Avatar, Box, Button, Typography } from '@mui/material';
-import LoginIcon from '@mui/icons-material/Login';
+import SignLayout from "../layout/SignLayout";
 
-import SignLayout from '../layout/SignLayout';
-import TextFieldComp from '../components/TextFieldComp';
-
-import '../css/pages/login.scss';
+import theme from "../components/Theme";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
-
   const { loginErrorMessage } = useSelector((state) => state.user);
 
-  useEffect(() => {
+  const [id, setId] = React.useState("");
+  const [pw, setPw] = React.useState("");
+
+  React.useEffect(() => {
     dispatch(userSlice.actions.initLoginErrorMsg());
   }, []);
 
-  const onChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-
-    if (name === 'id') {
-      setId(value);
-    } else {
-      setPw(value);
-    }
+  const handleChangeId = (e) => {
+    setId(e.target.value);
   };
 
-  const onSubmit = () => {
+  const handleChangePw = (e) => {
+    setPw(e.target.value);
+  };
+
+  const handleSubmit = () => {
     const payload = { data: { id, pw }, navigate };
     dispatch(login(payload));
   };
 
   const onEnterPress = (e) => {
-    if (e.key === 'Enter') {
-      onSubmit();
+    if (e.key === "Enter") {
+      handleSubmit();
     }
   };
 
   return (
     <>
       <SignLayout>
-        <Box
-          onKeyPress={onEnterPress}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: '#1976D2' }}>
-            <LoginIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
-            로그인
-          </Typography>
-          <Box>
-            <TextFieldComp
-              id="id"
-              label="아이디"
-              name="id"
-              type="text"
-              ok={true}
-              onChange={onChange}
-              autoFocus={true}
-            />
-            <TextFieldComp
-              id="password"
-              label="비밀번호"
-              name="password"
-              type="password"
-              ok={true}
-              onChange={onChange}
-            />
-            <Button
-              type="submit"
-              variant="contained"
-              fullWidth
-              ok={true}
-              onClick={onSubmit}
-              sx={{ mt: 3, fontSize: 20, fontWeight: 700 }}
-            >
+        <ThemeProvider theme={theme}>
+          <Box
+            onKeyPress={onEnterPress}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: theme.palette.success.main }}>
+              <LoginIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
               로그인
-            </Button>
-            <Box component="span" sx={{ fontSize: 12, color: '#e74c3c' }}>
-              {loginErrorMessage}
+            </Typography>
+            <Box>
+              <TextField
+                label="아이디"
+                type="text"
+                color="success"
+                onChange={handleChangeId}
+                autoFocus={true}
+                required={true}
+                autoComplete="off"
+                fullWidth
+              />
+              <TextField
+                label="비밀번호"
+                type="password"
+                color="success"
+                onChange={handleChangePw}
+                autoFocus={true}
+                required={true}
+                fullWidth
+                sx={{ mt: 2 }}
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="success"
+                fullWidth
+                ok={true}
+                onClick={handleSubmit}
+                disabled={!id || !pw}
+                sx={{ mt: 3, fontSize: 20, fontWeight: 700 }}
+              >
+                로그인
+              </Button>
+              <Box
+                component="span"
+                sx={{ fontSize: 12, color: theme.palette.error.main }}
+              >
+                {loginErrorMessage}
+              </Box>
+            </Box>
+            <Box sx={{ mt: 1, fontSize: 13 }}>
+              아이디가 없나요? &nbsp;
+              <Link to="/sign-up">회원가입 하러가기 &rarr;</Link>
             </Box>
           </Box>
-          <Box sx={{ mt: 1, fontSize: 13, color: '#FFF' }}>
-            아이디가 없나요? &nbsp;
-            <Link to="/sign-up">회원가입 하러가기 &rarr;</Link>
-          </Box>
-        </Box>
+        </ThemeProvider>
       </SignLayout>
     </>
   );
