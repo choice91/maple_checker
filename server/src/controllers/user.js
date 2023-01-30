34010b70cf0a83ad49327eb54b8094cb5e517c84
LST <wer1264@gmail.com>
2023-01-30 16:26:19 +0900

    Add: (server) 이름, 비밀번호 변경 API 추가

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -1,7 +1,7 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-import db from '../models';
-import { signAccessToken, signRefreshToken } from '../utils/jwt';
+import db from "../models";
+import { signAccessToken, signRefreshToken } from "../utils/jwt";
 
 export default {
   signup: async (req, res) => {
@@ -10,21 +10,19 @@ export default {
     if (password !== password2) {
       res.status(400).json({
         ok: false,
-        type: 'password incorrect',
-        errorMessage: '비밀번호가 일치하지 않습니다.',
+        type: "password incorrect",
+        errorMessage: "비밀번호가 일치하지 않습니다.",
       });
       return;
     }
 
-    const exists = await db.User.exists({
-      id,
-    });
+    const exists = await db.User.exists({ id });
 
     if (exists) {
       res.status(400).json({
         ok: false,
-        type: 'exist id',
-        errorMessage: '이미 존재하는 아이디입니다.',
+        type: "exist id",
+        errorMessage: "이미 존재하는 아이디입니다.",
       });
       return;
     }
@@ -39,7 +37,7 @@ export default {
 
     res.status(200).json({
       ok: true,
-      message: '회원가입 성공',
+      message: "회원가입 성공",
     });
   },
 
@@ -53,14 +51,14 @@ export default {
     if (exists) {
       res.status(409).json({
         ok: false,
-        errorMessage: 'ID가 중복입니다.',
+        errorMessage: "ID가 중복입니다.",
       });
       return;
     }
 
     res.status(200).json({
       ok: true,
-      message: '사용할 수 있는 ID입니다.',
+      message: "사용할 수 있는 ID입니다.",
     });
   },
 
@@ -72,7 +70,7 @@ export default {
     if (!user) {
       res.status(404).json({
         ok: false,
-        errorMessage: '존재하지 않는 유저입니다.',
+        errorMessage: "존재하지 않는 유저입니다.",
       });
       return;
     }
@@ -82,7 +80,7 @@ export default {
     if (!pwCompare) {
       res.status(400).json({
         ok: false,
-        errorMessage: '다시 시도해주세요',
+        errorMessage: "다시 시도해주세요",
       });
       return;
     }
@@ -95,7 +93,7 @@ export default {
 
     res.status(200).json({
       ok: true,
-      message: '로그인 성공',
+      message: "로그인 성공",
       token: {
         accessToken,
         refreshToken,
@@ -113,7 +111,7 @@ export default {
     if (!user) {
       res.status(404).json({
         ok: false,
-        errorMessage: '존재하지 않는 유저',
+        errorMessage: "존재하지 않는 유저",
       });
       return;
     }
@@ -121,6 +119,52 @@ export default {
     res.status(200).json({
       ok: true,
       user,
+    });
+  },
+
+  updateUserProfile: async (req, res) => {
+    const {
+      user: { id: loginUserId },
+      body: { name, curPw, newPw, verifyPw },
+    } = req;
+
+    if (newPw !== verifyPw) {
+      res.status(400).json({
+        ok: false,
+        errorMessage: "변경할 비밀번호가 일치하지 않음",
+      });
+      return;
+    }
+
+    const user = await db.User.findById(loginUserId);
+
+    if (!user) {
+      res.status(404).json({
+        ok: false,
+        errorMessage: "존재하지 않는 유저",
+      });
+      return;
+    }
+
+    const pwCompare = await bcrypt.compare(curPw, user.password);
+
+    if (!pwCompare) {
+      res.status(400).json({
+        ok: false,
+        errorMessage: "현재 비밀번호가 일치하지 않음",
+      });
+      return;
+    }
+
+    const newHashedPassword = await bcrypt.hash(newPw, 12);
+    await db.User.updateOne(
+      { _id: loginUserId },
+      { name, password: newHashedPassword }
+    );
+
+    res.status(200).json({
+      ok: true,
+      message: "변경완료",
     });
   },
 };
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,13 +1,14 @@
-import express from 'express';
+import express from "express";
 
-import { asyncHandler, authJWT } from '../middlewares';
-import userCtrl from '../controllers/user';
+import { asyncHandler, authJWT } from "../middlewares";
+import userCtrl from "../controllers/user";
 
 const userRouter = express.Router();
 
 userRouter
-  .route('/profile')
+  .route("/profile")
   .all(authJWT)
-  .get(asyncHandler(userCtrl.getUserProfile));
+  .get(asyncHandler(userCtrl.getUserProfile))
+  .put(asyncHandler(userCtrl.updateUserProfile));
 
 export default userRouter;
